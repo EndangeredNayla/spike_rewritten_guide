7401dbdb2b38b0f603592951bd21c3392d7373ba
Courtney Ripoll <courtney.t.ripoll@gmail.com>
2019-11-02 20:14:46 -0400

    updating settings for production

--- rewritten_guide/Program.cs
+++ rewritten_guide/Program.cs
@@ -19,6 +19,7 @@ namespace rewritten_guide
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .Build();
     }
