86aa87234a6b744c4e494d6e72bb97e4406a7c99
Courtney Ripoll <courtney.t.ripoll@gmail.com>
2019-12-09 22:41:41 -0500

    Updates

---
+++ js/back-to-top.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+$(document).on("scroll", function() {
+    if($(document).scrollTop() > window.innerHeight) {
+        $("#backtotop-link").addClass("show");
+    } else {
+        $("#backtotop-link").removeClass("show");
+    }
+});
---
+++ precache-manifest.631737d4bb9e5de64c6f4ffbb3bcbccf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d146b373e16718684e0fb94b99658de1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "979574ee1bba648087ef",
+    "url": "/static/css/main.8a390f3f.chunk.css"
+  },
+  {
+    "revision": "506e9e7633673d70373a",
+    "url": "/static/js/2.f380a1c5.chunk.js"
+  },
+  {
+    "revision": "979574ee1bba648087ef",
+    "url": "/static/js/main.bb488c6d.chunk.js"
+  },
+  {
+    "revision": "a580033616dc97b51dc6",
+    "url": "/static/js/runtime-main.8c86bbbd.js"
+  }
+]);
